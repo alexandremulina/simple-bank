36f0caa34bd7fa0b9acad7799c40055423f40a2d
alexandremulina <alexandremulina@hotmail.com>
2023-10-05 23:46:38 -0300

    simple-bank

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	
+	"log"
+)
+
+func main() {
+    name := "Go Developers"
+    log.Println("Azure for", name)
+}
---
+++ util/random.go
@@ -0,0 +1,41 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+func init(){
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min,max int64) int64{
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string{
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	s := make([]rune,n)
+	for i := range s{
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
+}
+
+//RandomOwner returns a random owner name
+func RandomOwner() string{
+	return RandomString(6)
+}
+
+//RandomMoney returns a random amount of money
+func RandomMoney() float64 {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Float64() * 1000
+}
+
+//RandomCurrency returns a random currency code
+func RandomCurrency() string{
+	currencies := []string{"USD","EUR","CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
